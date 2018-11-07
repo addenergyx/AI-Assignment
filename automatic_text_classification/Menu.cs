f0260fa4cfd0ff9ff4f0fc815f219269a0ab3a5e
David <david-adeniji@hotmail.co.uk>
2018-11-07 01:24:50 +0000

    tf-idf for labour

--- automatic_text_classification/Menu.cs
+++ automatic_text_classification/Menu.cs
@@ -241,6 +241,7 @@ namespace automatic_text_classification
                         break;
 
                     case 3:
+                        Title();
 
                         pathToDir = PathToDirectory(); //path to directory containing training data
                         while (!Directory.Exists(pathToDir)) //throw new ArgumentException("File doesn't exist, enter new path")
@@ -251,27 +252,224 @@ namespace automatic_text_classification
                             pathToDir = "training_dataset"; //gets file from debug/bin - for testing purposes at the moment
                         }
 
-                        stopWordsFile = Doc.FileExists(PathToStopWords(), "stop words file"); //Stopwords lookup table
+                        //stopWordsFile = Doc.FileExists(PathToStopWords(), "stop words file"); //Stopwords lookup table
+                        stopWordsFile = "stopwords.txt"; //for testing purposes
 
                         Console.WriteLine("Training datasets: " + Doc.FileCount(pathToDir));
 
                         files = Directory.GetFiles(pathToDir);
 
-                        Title();
+                        foreach (string file in files)
+                        {
+                            //Building government dictionary used to keep track of number of datasets for each government
+                            if (governmentDict.ContainsKey(Doc.DocGovernment(file))) { governmentDict[Doc.DocGovernment(file)]++; }
+                            else { governmentDict.Add(Doc.DocGovernment(file), 1); }
+                        }
+
+                        foreach (string file in files)
+                        {
+                            string government = Doc.DocGovernment(file); //Government of file
+                            priorProbability = Calculations.PriorProbabilities(government, Doc.FileCount(pathToDir), governmentDict);
+
+                            // key-value pair word frequency
+                            wordCount = Calculations.WordFrequency(file, dict, stopWordsFile);
+
+                            switch (government)
+                            {
+                                case nameof(Doc.Government.Conservative):
+                                    conTotal += wordCount; // Total number of words in each category including repeats
+                                    conDict = conDict.Union(dict).GroupBy(i => i.Key, i => i.Value).ToDictionary(i => i.Key, i => i.Sum());
+                                    conPriorProbability = priorProbability;
+                                    break;
+                                case nameof(Doc.Government.Coalition):
+                                    coaTotal += wordCount;
+                                    coaDict = coaDict.Union(dict).GroupBy(i => i.Key, i => i.Value).ToDictionary(i => i.Key, i => i.Sum());
+                                    coaPriorProbability = priorProbability;
+                                    break;
+                                case nameof(Doc.Government.Labour):
+                                    labTotal += wordCount;
+                                    labDict = labDict.Union(dict).GroupBy(i => i.Key, i => i.Value).ToDictionary(i => i.Key, i => i.Sum());
+                                    labPriorProbability = priorProbability;
+                                    break;
+                                default:
+                                    Console.WriteLine("Could not determine government, data will be discarded");
+                                    Console.ReadLine();
+                                    break;
+                            }
+                            //uniqueDict = uniqueDict.Union(dict).GroupBy(i => i.Key, i => i.Value).ToDictionary(i => i.Key, i => i.Sum());
+
+                        }
+
+                        ////////////////////////////////// KEEP ABOVE
+
+                        /*
+                        nWords = uniqueDict.Count(); //Total number of unique words throughout training documents
+
+                        //After talk in class realised need to keep words that don't appear in a category and set value to 0
+                        //To get cat[word] = 0 for words not in a category need to comapre uniqueDict to (category)Dict
+                        foreach (KeyValuePair<string, int> word in uniqueDict)
+                        {
+                            //Expert system technique, for each word all conditions are triggered but only fired if false
+                            if (!coaDict.ContainsKey(word.Key)) { coaDict.Add(word.Key, 0); }
+                            if (!labDict.ContainsKey(word.Key)) { labDict.Add(word.Key, 0); }
+                            if (!conDict.ContainsKey(word.Key)) { conDict.Add(word.Key, 0); }
+                        }
 
                         //First work out term frequency in category - have a dictionary of word and frequency
                         //only need dict of test to get the words
 
-
-
-                        foreach (string fileName in files)
+                        foreach (KeyValuePair<string, int> fcat in conDict) //fcat refers to frequency of word in given category
                         {
-                            if (Doc.DocGovernment(fileName) == Doc.Government.Labour.ToString())
+                            double cp = Calculations.ConditionalProbability(fcat.Value, conTotal, nWords);
+                            concpdict.Add(fcat.Key, cp); // Building conditional probability dictionary
+                        }
+                        foreach (KeyValuePair<string, int> fcat in coaDict)
+                        {
+                            double cp = Calculations.ConditionalProbability(fcat.Value, coaTotal, nWords);
+                            coacpdict.Add(fcat.Key, cp);
+                        }
+                        foreach (KeyValuePair<string, int> fcat in labDict)
+                        {
+                            double cp = Calculations.ConditionalProbability(fcat.Value, labTotal, nWords);
+                            labcpdict.Add(fcat.Key, cp);
+                        }
+                        */
+                        ///////////////////////////////////////////////////
+
+                        //pathToTest = Doc.FileExists(PathToTestDocument(), "test document");
+                        pathToTest = "test_dataset/test1.txt"; //for testing purposes
+                        Calculations.WordFrequency(pathToTest, fileDict, stopWordsFile);
+
+                        //List<double> labtfidf = new List<double>();
+
+                        Dictionary<string, double> labTFIDF = new Dictionary<string, double>();
+
+                        foreach (var word in fileDict)
+                        {
+                            List<double> labWordTFIDF = new List<double>();
+
+                            foreach (string file in files)
                             {
+                                int wordExistInFileCount = 0;
+
+                                if (Doc.DocGovernment(file) == Doc.Government.Labour.ToString())
+                                {
+                                    Dictionary<string, int> tempDict = new Dictionary<string, int>();
+                                    wordCount = Calculations.WordFrequency(file, tempDict, stopWordsFile);
+
+                                    //foreach (var word in fileDict)
+                                    //{
+
+                                    // Term frequency = count how many times the term appears in this document.
+
+                                    tempDict.TryGetValue(word.Key, out int frequency);
+
+                                    double tf = frequency / (double)wordCount; //to get floating point arithmetic atleast one variable must be a double
+
+                                        foreach (string doc in files)
+                                        {
+                                            if (Doc.DocGovernment(doc) == Doc.Government.Labour.ToString())
+                                            {
+                                                Dictionary<string, int> tempDict2 = new Dictionary<string, int>();
+                                                Calculations.WordFrequency(doc, tempDict2, stopWordsFile);
+                                                if (tempDict2.ContainsKey(word.Key)) { wordExistInFileCount++; }
+                                            }
+
+                                        }
+                                    // Calculate the IDF for each word
+                                    double idf = 0D;
+                                    //For unseen words must use smoothed inverse document frequency techniques as cannot divide a number by 0 (add 1 to wordExistInFileCount)
+                                    //idf = Math.Log(governmentDict["Labour"] / ((double)1 + wordExistInFileCount));  //if a word appears in all docs of a category then idf will be 1 (lower bound for IDF) as that word is not considered special
+
+                                    //Term inverse document requency - number of documents in a category that word appears in
+                                    // IDF is log(number of doc in category/no of doc with that term)
+                                    if (wordExistInFileCount == 0) { idf = 1; } //For unseen words must use smoothed inverse document frequency techniques as cannot divide a number by 0
+                                    else { idf = 1 + Math.Log(governmentDict["Labour"] / (double)wordExistInFileCount); } //if a word appears in all docs of a category then idf will be 1 (lower bound for IDF) as that word is not considered special
+
+                                    // Calculate TFIDF
+                                    double tfidf = tf * idf;
+                                    labWordTFIDF.Add(tfidf);// repeat procedure for other appearances of word in other docs in category, will add together later 
+
+                                    //labTFIDF[word.Key] = tf * idf; 
+                                    //Thus tf-idf of word is:
+                                    //double tfidf = tf * idf;
+                                    //labtfidf.Add(tfidf);
+                                    //}
+                                }
 
                             }
+                            labTFIDF[word.Key] = labWordTFIDF.Sum();
+                            Console.WriteLine(word.Key + ": " + labTFIDF[word.Key]); //this is a slow process so printing out idf to give user feedback
+                        }
 
-                            if (MainClass.DocGovernment(fileName) == MainClass.Government.Coalition.ToString())
+
+                        List<double> catTFIDF = new List<double>();
+
+
+                        foreach (var word in labDict)
+                        {
+                            List<double> labWordTFIDF = new List<double>();
+
+                            foreach (string file in files)
+                            {
+                                int wordExistInFileCount = 0;
+
+                                if (Doc.DocGovernment(file) == Doc.Government.Labour.ToString())
+                                {
+                                    Dictionary<string, int> tempDict = new Dictionary<string, int>();
+                                    wordCount = Calculations.WordFrequency(file, tempDict, stopWordsFile);
+
+                                    tempDict.TryGetValue(word.Key, out int frequency); //if not found frequency is 0
+
+                                    double tf = frequency / (double)wordCount; //to get floating point arithmetic atleast one variable must be a double
+
+                                    foreach (string doc in files)
+                                    {
+                                        if (Doc.DocGovernment(doc) == Doc.Government.Labour.ToString())
+                                        {
+                                            Dictionary<string, int> tempDict2 = new Dictionary<string, int>();
+                                            Calculations.WordFrequency(doc, tempDict2, stopWordsFile);
+                                            if (tempDict2.ContainsKey(word.Key)) { wordExistInFileCount++; }
+                                        }
+
+                                    }
+                                    // Calculate the IDF for each word
+                                    double idf = 0D;
+
+                                    //Term inverse document requency - number of documents in a category that word appears in
+                                    if (wordExistInFileCount == 0) { idf = 1; } 
+                                    else { idf = 1 + Math.Log(governmentDict["Labour"] / (double)wordExistInFileCount); } 
+
+                                    // Calculate TFIDF
+                                    double tfidf = tf * idf;
+                                    labWordTFIDF.Add(tfidf); 
+
+                                    //labTFIDF[word.Key] = tf * idf; 
+                                    //Thus tf-idf of word is:
+                                    //double tfidf = tf * idf;
+                                    //labtfidf.Add(tfidf);
+                                    //}
+                                }
+
+                            }
+                            catTFIDF.Add(labWordTFIDF.Sum()); //list containing tf-idf weights of all words in category
+                            Console.WriteLine(word.Key + ": " + labWordTFIDF.Sum()); //this is a slow process so printing out idf to give user feedback
+                        }
+
+                        double nCat = catTFIDF.Sum();
+                        Dictionary<string, double> catCpTFIDF = new Dictionary<string, double>();
+
+                        foreach (var word in labTFIDF)
+                        {
+                            //tf-idf uses same conditional probability formula
+                            double cp = Calculations.ConditionalProbability(word.Value, nCat, catTFIDF.Count());
+                            catCpTFIDF.Add(word.Key, Math.Log(cp));
+                        }
+
+                        double prob = catCpTFIDF.Sum(x => x.Value);
+
+                            /*
+                            if (Doc.DocGovernment(file) == Doc.Government.Coalition.ToString())
                             {
                                 foreach (var word in fileDict)
                                 {
@@ -279,7 +477,7 @@ namespace automatic_text_classification
                                 }
                             }
 
-                            if (MainClass.DocGovernment(fileName) == MainClass.Government.Conservative.ToString())
+                            if (Doc.DocGovernment(file) == Doc.Government.Conservative.ToString())
                             {
                                 foreach (var word in fileDict)
                                 {
@@ -287,7 +485,7 @@ namespace automatic_text_classification
                                 }
                             }
                         }
-
+                        */
 
                         //Term inverse document requency - number of documents in a category that word appears in
                         // log(number of doc in category/no of doc with that term)
